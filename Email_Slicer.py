4f4204823747fe2595bc9acefe11559ca1a163a5
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-02-28 00:43:51 +0000

    second commit

--- Email_Slicer.py
+++ Email_Slicer.py
@@ -3,12 +3,24 @@ emailAddress = input("Enter your email address! ")
 checker = True
 sign = '@'
 period = '.'
+username = ""
+domain = ""
+counter = 0
+locationOfSign = 0
 
-while checker:
-    if sign or period in emailAddress:
-        break;
+while True:
+    if sign and period in emailAddress:
+        break
     else:
         input("That is not a valid Email Address! Please enter a valid one. ")
         continue
 
+while counter<len(emailAddress):
+    if emailAddress[counter] == "@":
+        counter += 1
+    elif counter>locationOfSign:
+        domain += emailAddress[counter]
+    else:
+        username += emailAddress[counter]
 
+print("Your username is " + username + ". Your domain is " + domain + ". ")
